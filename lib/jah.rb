ab4c931cae481dbe84f04c5fba466b2d5654a2e4
Marcos Piccinini <x@nofxx.com>
2011-04-15 00:09:51 -0300

    ugly working version

--- lib/jah.rb
+++ lib/jah.rb
@@ -1,20 +1,20 @@
-require 'rubygems'
-require 'optparse'
+require "rubygems"
+require "optparse"
 begin
-  require 'i18n'
+  require "i18n"
 rescue LoadError
   puts "Gem 'i18n' not found, try `gem install i18n`"
   exit
 end
-#autoload :Drb, 'drb'
-require 'jah/opt'
-require 'jah/cli'
-require 'jah/install'
-require 'jah/agent'
-require 'jah/history'
-autoload :God, 'god'
+#autoload :Drb, "drb"
+require "jah/opt"
+require "jah/cli"
+require "jah/install"
+require "jah/agent"
+require "jah/history"
+autoload :God, "god"
 autoload :Prayer, "jah/prayer"
-require 'jah/command'
+require "jah/command"
 %w{mem cpu who disk prok netstat admin status extra packages}.each do |inc|
   require "jah/commands/#{inc}"
 end
@@ -23,6 +23,6 @@ require "jah/act_pkg/base"
 require "jah/act_pkg/pkg"
 
 module Jah
-  VERSION =  File.read(File.join(File.dirname(__FILE__), '..', 'VERSION'))
-  I18n.load_path += Dir[File.join(File.dirname(__FILE__), 'locales', "*.{rb,yml}")]
+  VERSION =  File.read(File.join(File.dirname(__FILE__), "..", "VERSION"))
+  I18n.load_path += Dir[File.join(File.dirname(__FILE__), "locales", "*.{rb,yml}")]
 end
--- lib/jah/act_pkg/base.rb
+++ lib/jah/act_pkg/base.rb
@@ -1,6 +1,33 @@
 module Jah
   module ActPkg
+ autoload :Pacman, "jah/act_pkg/pacman"
 
+    def self.detect
+      if RUBY_PLATFORM =~ /darwin/
+        :ports
+      else
+        case File.read("/etc/issue")
+        when /Arch/i    then :pacman
+        when /SUSE/i    then :zypp
+        when /Mandriva/ then :urpm
+        when /BSD/      then :ports
+        when /Fedora|CentOS/ then :yum
+        when /Debian|Ubuntu/ then :apt
+        when /This|Gentoo/   then :emerge
+        when /Welcome|Slack/ then :slack
+        else
+          raise "Is this LFS??"
+        end
+      end
+    end
+
+    def self.manager
+      @manager ||= const_get(detect.to_s.capitalize).new
+    end
+
+    def self.method_missing(*meth)
+      manager.send(*meth)
+    end
     class Base
 
 
